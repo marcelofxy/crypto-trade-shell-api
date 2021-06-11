08b6abba8cb2a3205962e8e24583f06fde15dbcb
marcelofxy <31291445+marcelofxy@users.noreply.github.com>
2021-06-11 20:06:40 -0300

    Update deribit.sh
    simple execution of private api order with curl in bash/shell

--- deribit.sh
+++ deribit.sh
@@ -4,7 +4,7 @@ id=miurei8g
 secret=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 uri="https://www.deribit.com/api/v2/public/auth?client_id=$id&client_secret=$secret&grant_type=client_credentials"
 instrumento="https://www.deribit.com/api/v2/private/buy?amount=1&instrument_name=ETH-16AUG20-425-P&label=market0000234&type=market"
-
+ 
 bearer=$(curl -X GET $uri -H "Content-Type: application/json")
 
 echo $bearer
