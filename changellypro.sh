a712b10965361a7adc60cf9c625317db8507fe83
marcelofxy <31291445+marcelofxy@users.noreply.github.com>
2021-06-11 20:27:05 -0300

    Create changellypro.sh
    Changelly Pro - Simple execution of private api order with curl in bash/shell.

---
+++ changellypro.sh
@@ -0,0 +1,8 @@
+## INSTALL uuidgen
+
+uid=$(uuidgen -t | cut -c 1-32)
+
+## insert key:secret
+curl -X PUT -u "XXXXXXXXXXXXXXXXXXXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXX" \
+    "https://api.pro.changelly.com/api/2/order/$uid" \
+    -d 'symbol=ETHUSDT&side=sell&type=market&quantity=1'
